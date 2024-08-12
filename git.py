76521f9a299a6aa619cf390494f9ef424641844c
Navaj66 <mullanavj66@gmail.com>
2024-08-12 13:12:27 +0530

    Else statement is added

---
+++ git.py
@@ -0,0 +1,20 @@
+print("Hello")
+#calculator
+Num1=int(input("Enter no.1= "))
+operator=input("Enter operator(+,-,*,/) = ")
+Num2=int(input("Enter no.2= "))
+
+if operator== "+":
+    print(Num1 + Num2)
+
+elif operator== "-":
+    print(Num1 - Num2)
+
+elif operator== "*":
+    print(Num1 * Num2)
+
+elif operator== "/":
+    print(Num1 / Num2)
+
+else:
+    print( "Enter valid Opeartor" )
